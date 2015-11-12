4534e6d69ace1d8ac068d130b3b7215d7021df14
Dave M <dave@davem.com>
2015-11-11 16:21:39 -0800

    adding some android dev

---
+++ davem/install.android.dev.environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install npm
+sudo npm install -g cordova@3.6.3-0.2.13
+sudo apt-get install default-openjdk
+echo http://developer.android.com/sdk/index.html#top
